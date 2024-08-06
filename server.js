ee2ae02b01bd01b5cde527d6e645c6c6cdcf0e39
Thejvanth <thejvanth08@gmail.com>
2024-08-06 21:01:01 +0530

    Change code into rate-limiter

--- server.js
+++ server.js
@@ -4,23 +4,30 @@ const cors = require("cors");
 const port = process.env.PORT || 3000;
 const app = express();
 
-// app.use(rateLimiter({
-//   windowMs: 5 * 60 * 1000,
-//   limit: 10,
-//   message: "You exceeded your request limit"
-// }));
+app.use(
+  rateLimiter({
+    windowMs: 5 * 60 * 1000,
+    limit: 10,
+    message: "You exceeded your request limit (10)",
+  }),
+);
 
-app.use(cors({
-  origin: "*"
-}));
+app.use(
+  cors({
+    origin: "*",
+  }),
+);
+
+let count = 0;
 
 // Trust the first proxy
-app.set('trust proxy', true);
+app.set("trust proxy", true);
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   // Get the client's IP address
-  const clientIp = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-  res.send(`Client IP address: ${clientIp}`);
+  // const clientIp = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+  // res.send(`Client IP address: ${clientIp}`);
+  res.send(`your request count ${count++}`);
 });
 const server = app.listen(port, console.log("server is running at port", port));
 
